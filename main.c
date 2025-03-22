b5c0783448e508668a872173ff7ca9c99a8abec3
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-22 17:14:30 -0400

    Set up GLFW probably

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include "GLFW/glfw3.h"
+// #include <thread>
 
-// gcc main.c
+// gcc main.c -framework OpenGL
+// -o
 // ./a.out
+
 // I really don't wanna make a makefile
+// https://open.gl/
+// we gon use GLFW
 
-int main() {
+// int main() {
 
-	printf("Hello world! Idk how OpenGL works\n");
+// 	printf("Hello world! Idk how OpenGL works\n");
 
-	return 0;
+// 	return 0;
+// }
+
+int main()
+{
+	int myvar = 0;
+
+    glfwInit();
+
+	scanf("%d", &myvar);
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // glfwTerminate();
 }
