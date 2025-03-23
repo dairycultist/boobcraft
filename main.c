5a68b195ab7bf2ee5516b6757b0d2daf1446be09
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-22 21:22:55 -0400

    Draw a triangle

--- main.c
+++ main.c
@@ -9,10 +9,13 @@
 // I really don't wanna make a makefile
 // https://open.gl/
 
+// OpenGL/GLFW/GLEW
+
 int main() {
 
-	printf("Hello world! Idk how OpenGL/GLFW/GLEW works\n");
-	// printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+/************************************
+	initialize GLFW, window, and GLEW
+	*/
 
     glfwInit();
 
@@ -21,25 +24,75 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL); // Windowed
-	// GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
-
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer);
+	printf("OpenGL version: %s\n", glGetString(GL_VERSION));
 
-	printf("%u\n", vertexBuffer);
+/* send vertices to graphics card via VBO */
+	
+	// create buffer (returns index)
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
 
+	// make this buffer the active object
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	float vertices[] = {
+		0.0f,  0.5f, // Vertex 1 (X, Y)
+		0.5f, -0.5f, // Vertex 2 (X, Y)
+	   -0.5f, -0.5f  // Vertex 3 (X, Y)
+	};
+
+	// copy vertex data to active buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+/* create shader program and send to graphics card */
+
+	const char* vertexSource = "#version 150 core \n in vec2 position; void main() { gl_Position = vec4(position, 0.0, 1.0); }";
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	const char* fragmentSource = "#version 150 core \n out vec4 outColor; void main() { outColor = vec4(1.0, 1.0, 1.0, 1.0); }";
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+/* link between vertex data and attributes */
+
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
+
+/* loop */
 	while (!glfwWindowShouldClose(window)) {
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+
+
+
+/* terminate program */
     glfwTerminate();
 }
