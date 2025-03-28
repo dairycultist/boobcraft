f258b5e56e4752b9183f9945d1448f9be887bfa8
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-27 23:37:56 -0400

    Depth lighting

--- fragment.glsl
+++ fragment.glsl
@@ -1,9 +1,10 @@
 #version 150 core
 
 in vec3 color;
+in float depth;
 
 out vec4 outColor;
 
 void main() {
-    outColor = vec4(color, 1.0);
+    outColor = vec4(1 - depth * 0.1, 1 - depth * 0.1, 1 - depth * 0.1, 1.0);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -5,8 +5,11 @@ uniform vec3 player_pos;
 in vec3 position;
 
 out vec3 color;
+out float depth;
 
 void main() {
     color = vec3(position.x * 2, 0.5, position.y * 2);
+
     gl_Position = vec4(position.xy - player_pos.xy, 0.0, position.z - player_pos.z);
+    depth = gl_Position.w;
 }
