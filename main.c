646706ce0dcea2d4ebf286dcd2f85b9d7d918ae2
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-06 00:09:06 -0400

    Fancy graphics test

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 #include "draw.c"
 
 int main() {
@@ -11,25 +12,20 @@ int main() {
 
 	while (i < 1000) {
 
-		for (y = 0; y < HEIGHT; y += 2) {
+		for (y = 0; y < HEIGHT; y++) {
 
 			for (x = 0; x < WIDTH; x++) {
 
-				if (x < 20) {
-					R[x][y] = (i / 6) % 6;
-					R[x][y+1] = (i / 6) % 6;
-					G[x][y] = i % 6;
-					G[x][y+1] = i % 6;
-					B[x][y] = (i / 36) % 6;
-					B[x][y+1] = (i / 36) % 6;
+				int dist = sqrt(pow(x - 40, 2) + pow(y - 20, 2));
+
+				if (dist < 20) {
+					R[x][y] = abs((i - dist) % 10 - 5);
 				}
 			}
 		}
 
 		draw_buffer();
-
 		i++;
-
 		usleep(50000); // this is in MICROSECONDS (1/1000 of a millisecond)
 	}
 
